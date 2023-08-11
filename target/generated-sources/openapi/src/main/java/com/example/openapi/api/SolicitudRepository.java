4bb82281e75b18320d0afb33b0395e72654a85e8
gustavo <gustavoeguanuco@gmail.com>
2023-08-11 01:44:36 -0300

    agregado de comentario al codigo

--- target/generated-sources/openapi/src/main/java/com/example/openapi/api/SolicitudRepository.java
+++ target/generated-sources/openapi/src/main/java/com/example/openapi/api/SolicitudRepository.java
@@ -10,15 +10,26 @@ import com.example.openapi.model.Solicitud;
 
 public interface SolicitudRepository extends JpaRepository<Solicitud, Integer> {
 
+	/*
+	 * OBTENER LAS SOLICITUDES: con legajo del solicitante y tambien con el legajo del agente que responde
+	 */
 	@Query("SELECT s FROM Solicitud s LEFT JOIN Respuesta r ON r.idSolicitud= s.idSolicitud WHERE s.legajoSolicitud = ?1")
 	List<Solicitud> findByLegajoSolicitud(int legajo_solicitante);
 	
 	@Query("SELECT s FROM Solicitud s LEFT JOIN Respuesta r ON r.idSolicitud= s.idSolicitud WHERE s.legajoSolicitud = ?1 AND r.legajoRespuesta = ?2")
 	List<Solicitud> findByLegajoRespuesta(int legajo_solicitante, int legajo_respuesta);
 	
+	
+	/*
+	 * OBTENER EL NUMERO DE SOLICITUDES NUEVAS sin responder
+	 */
 	@Query("SELECT COUNT(s.idSolicitud) AS cantidad FROM Solicitud s LEFT JOIN Respuesta r ON s.idSolicitud = r.idSolicitud WHERE r.respuesta IS null")
 	Long getNumNuevas();
 	
+	
+	/*
+	 * OBTENER LAS SOLICITUDES RECHAZADAS: todas o con legajo solicitante
+	 */
 	@Query("SELECT s FROM Solicitud s LEFT JOIN Respuesta r ON r.idSolicitud = s.idSolicitud WHERE r.respuesta = 1")
 	List<Solicitud> getSolicitudesRechazadas();
 	
